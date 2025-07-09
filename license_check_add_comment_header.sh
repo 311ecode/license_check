48cc8829038ef98d6974e9b798b225b39b900950
Imre Toth <tothimre@gmail.com>
2025-07-10 01:32:47 +0200

    Enhance license header insertion logic for bash and comment files
    - Improve bash header script to handle files with/without shebang
      - Insert license after first line for files with shebang
      - Insert at beginning for files without shebang
    - Simplify comment header script by removing conditional logic
    - Ensure consistent newline handling in both scripts
    
    uuid-stamp: 948f2e22-9a44-44a5-9516-b7d79373dc44

--- license_check_add_comment_header.sh
+++ license_check_add_comment_header.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Copyright © 2025 Imre Toth <tothimre@gmail.com> - Proprietary Software. See LICENSE file for terms.
+
 license_check_add_comment_header() {
     local file="$1"
-        sed -i "1i\\// $LICENSE_HEADER" "$file"
-    fi
+    
+    # Add // style comment header at the beginning of the file
+    sed -i "1i\\// $LICENSE_HEADER" "$file"
 }
